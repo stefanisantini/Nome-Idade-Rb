44a43eb3d81f507386817027c1365f407be6d6bf
Stéfani Santini Reis <110628639+stefanisantini@users.noreply.github.com>
2023-03-06 22:48:27 -0300

    Add files via upload
    Programa com nome e idade com exibição dos dados na mesma frase

---
+++ idno.rb
@@ -0,0 +1,13 @@
+# saida de dado
+print 'Escreva sua nome: '
+
+# recebendo uma entrada no teclado
+nome = gets.chomp
+
+print 'Escreva sua idade: '
+
+idade = gets.chomp
+
+# saida utilizando puts
+# use codigo ruby dentro de uma string com #{code}
+puts "Hello #{nome} sua idade é #{idade}"
